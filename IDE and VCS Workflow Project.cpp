487b2233089116251e6205d6af105bc7e6ab6beb
Sally Schaffner <schaffners@vpcc.edu>
2025-05-30 19:36:16 -0400

    Add project files.

---
+++ IDE and VCS Workflow Project.cpp
@@ -0,0 +1,14 @@
+// IDE and VCS Workflow Project.cpp
+// This program displays your initials.
+
+#include <iostream>
+using namespace std;
+int main() {
+	char first, middle, last
+		// Store your initials in these variables
+		first = 'S';
+	middle = 'K';
+	last = 'S';
+	cout << first << middle << last << endl;
+	return 0;
+}
