02a3eff65f456c64cc03e246270657e3490bae82
Sally Schaffner <schaffners@vpcc.edu>
2025-06-01 16:21:26 -0400

    Added a Program 2.

--- IDE and VCS Workflow Project.cpp
+++ IDE and VCS Workflow Project.cpp
@@ -1,14 +1,15 @@
 // IDE and VCS Workflow Project.cpp
-// This program displays your initials.
-
+// This program displays your name, address, and phone #
 #include <iostream>
 using namespace std;
 int main() {
-	char first, middle, last
-		// Store your initials in these variables
-		first = 'R';
-	middle = 'J';
-	last = 'E';
-	cout << first << middle << last << endl;
+	string name, address, phoneNumber;
+	// Store your inforation in these variables
+	name = "Sally Schaffner";
+	address = "123 Main St.";
+	phoneNumber = "757-123-4567";
+	cout << name << endl;
+	cout << address << endl;
+	cout << phoneNumber << endl;
 	return 0;
 }
